0f2e17ce1766b4d5e1dc58a74748b620511f817b
weixiao <1015886344@qq.com>
2016-08-01 00:56:28 +0800

    更改文章列表显示原始内容，详情页转化md格式

--- routes/article.js
+++ routes/article.js
@@ -6,14 +6,12 @@ var router = express.Router();
 var auth = require('../middle'); //权限中间件
 var markdown = require('markdown').markdown;
 /* GET home page. */
-router.get('/list',auth.checkLogin, function(req, res) {
+router.get('/list', function(req, res) {
     Model('article').find().populate('user').exec(function(err,docs){
         if(err){
             console.log('查询文章失败');
         }else{
-            docs.forEach(function(doc){
-                doc.content = markdown.toHTML(doc.content);
-            });
+
             res.render('article/list',{title:'文章列表',articles:docs});
         }
 
@@ -34,25 +32,26 @@ router.post('/add', function(req, res) {
             res.redirect('back');
         }else{
             req.flash('success','发表文章成功');
-            res.redirect('/');
+            res.redirect('article/list');
         }
     })
 });
 
 //详情页
 router.get('/detail/:_id', function(req, res) {
-    console.log('11111111111111111111111');
-    console.log(req.params);
+
     Model('article').findById(req.params._id,function(err,doc){
+        doc.content = markdown.toHTML(doc.content);
         res.render('article/detail',{title:'文章详情',article:doc})
     })
 });
 
 router.get('/delete/:_id', function(req, res) {
-    console.log('delete-----------------');
+
     Model('article').findById(req.params._id,function(err,doc){
         if(doc){
-            if(req.session.user._id == doc.user){
+            //req.session.user 只有登入才有值
+            if(req.session.user && (req.session.user._id == doc.user)){
                 Model('article').remove({_id:req.params._id},function(err,doc){
                     req.flash('success','删除成功');
                     res.redirect('/');
