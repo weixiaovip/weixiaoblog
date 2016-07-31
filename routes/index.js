f8a4865f82fe6408abe24de9af8339b8e4e7976c
weixiao <1015886344@qq.com>
2016-08-01 00:20:09 +0800

    更改默认首页

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,8 @@ var router = express.Router();
 /* GET home page. */
 //首页
 router.get('/', function(req, res, next) {
-  //res.render('index', { title: '首页' });
-  res.redirect('/article/list');
+  res.render('index', { title: '首页' });
+  //res.redirect('/article/list');
 });
 
 router.get('/index.html', function(req, res, next) {
