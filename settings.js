4b76cf8c063ac3d43641fb41176c3b157cc2c1f2
weixiao <1015886344@qq.com>
2016-08-03 22:17:49 +0800

    修改为本地数据库

--- settings.js
+++ settings.js
@@ -4,5 +4,6 @@
 //系统的配置文件
 module.exports = {
     cookieSecret:'weixiao', //加密cookie的密钥
-    url:'mongodb://123.57.143.189:27017/weixiaoblog' //数据库地址
+    //url:'mongodb://123.57.143.189:27017/weixiaoblog' //老师的在线数据库地址
+    url:'mongodb://localhost:27017/weixiaoblog' //本地数据库地址
 };
